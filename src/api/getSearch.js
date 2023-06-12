704b6ea1d4ef264436937f3d68b72f0ff554cf3a
NicolayRusetsky <96424328+NicolayRusetsky@users.noreply.github.com>
2023-06-12 19:19:17 +0300

    Закончил основную работу

---
+++ src/api/getSearch.js
@@ -0,0 +1,16 @@
+const API_KEY = '37232515-9db0bbab388a0a64087dbc1a2';
+const BASE_URL = 'https://pixabay.com/api/';
+const PICS_ON_PAGE = 12;
+
+export const getSearch = (searchText, page) => {
+  const params = new URLSearchParams({
+    q: searchText,
+    page: page,
+    key: API_KEY,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    per_page: PICS_ON_PAGE,
+  });
+
+  return fetch(`${BASE_URL}?${params}`);
+};
